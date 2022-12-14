61a2c48ed75881f4f6f5c9ab8a4cba2efd3b04a5
LIFE-is-Fantastic88 <84429221+LIFE-is-Fantastic88@users.noreply.github.com>
2022-12-14 21:41:03 +0800

    Create paho-mqtt_cloudmqtt.py

---
+++ paho-mqtt_cloudmqtt.py
@@ -0,0 +1,25 @@
+from paho import paho.mqtt.client as mqttclient
+import time
+
+def on_connect(client,usedata,flags,rc):
+    if rc==0:
+        print("Client is connected")
+        global connected
+        connected = True
+    else:
+        print("Connection failed gai gai")
+connected = False
+broker_addressses="xxxxx.cloudmqtt.com"
+port=12551
+user="xxxxxxxxx"
+password="xxxxxxxxx"
+
+client=mqttclient.Client("MQTT")
+client.username_pw_set(user,password)
+client.on_connect=on_connect
+client.connect(broker_address, port=port)
+client.loop_start()
+while connected!=True:
+    time.sleep(0.2)
+client.publish("mqtt/firstcode","Hi sam")
+client.loop_stop()
